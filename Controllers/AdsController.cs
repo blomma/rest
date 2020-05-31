4166d2df26b1f27e0c79ac0bee9fc18a2869d415
Mikael Hultgren <blomma@artsoftheinsane.com>
2020-05-31 14:06:07 +0200

    Rudimentary paging
    branch paging # Changes to be committed: #	modified:
    Controllers/AdsController.cs #	modified:   Models/Filter.cs #	new file:
    Models/PagedCollectionResponse.cs #	modified:   Services/AdService.cs #

--- Controllers/AdsController.cs
+++ Controllers/AdsController.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using rest.Models;
 using rest.Exceptions;
 using rest.Services;
-using System.Linq;
 using System;
 
 namespace rest.Controllers {
@@ -18,11 +16,33 @@ namespace rest.Controllers {
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Ad>>> GetAds([FromQuery] Filter filter) {
+        public async Task<ActionResult<PagedResponse<Ad>>> GetAds([FromQuery] Filter filter) {
             try {
-                var ads = await adService.GetAds(filter);
+                var result = await adService.GetAds(filter);
 
-                return Ok(ads);
+                var pagedResult = new PagedResponse<Ad>();
+                pagedResult.Items = result.Ads;
+
+                // TODO: Functional, but factor out into something more generic once final
+                if (filter.Page != null && filter.Limit != null) {
+                    Filter nextFilter = filter.Clone() as Filter;
+                    nextFilter.Page = nextFilter.Page + 1;
+                    var nextLimit = nextFilter.Page * nextFilter.Limit;
+                    String nextUrl = nextLimit <= result.TotalCount
+                        ? Url.Action(null, null, nextFilter, Request.Scheme)
+                        : null;
+
+                    Filter previousFilter = filter.Clone() as Filter;
+                    previousFilter.Page = previousFilter.Page - 1;
+                    String previousUrl = previousFilter.Page > 0
+                        ? Url.Action("GetAds", null, previousFilter, Request.Scheme)
+                        : null;
+
+                    pagedResult.NextPage = !String.IsNullOrWhiteSpace(nextUrl) ? new Uri(nextUrl) : null;
+                    pagedResult.PreviousPage = !String.IsNullOrWhiteSpace(previousUrl) ? new Uri(previousUrl) : null;
+                }
+
+                return Ok(pagedResult);
             } catch (ArgumentOutOfRangeException e) {
                 throw new HttpResponseException {
                     Status = 400,
---
+++ Models/PagedCollectionResponse.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace rest.Models {
+    public class PagedResponse<T> where T : class {
+        public IEnumerable<T> Items { get; set; }
+        public Uri NextPage { get; set; }
+        public Uri PreviousPage { get; set; }
+    }
+}
